3da687dc333236826d681aa566fc84325dcbb326
alexkanga <kalexane@gmail.com>
2023-09-13 22:25:09 +0000

    Implementation de la fonction 2

--- script.js
+++ script.js
@@ -4,14 +4,45 @@ function countWords(phrase,nombre) {
         arr = phrase.split(' ');
         for(let i=0; i<arr.length; i++) {
 
-            if(typeof arr[i] === 'string'){
+            if(typeof arr[i] === 'string' && arr[i].length === nombre) {
                 wordArray.push(arr[i]);
             }
+
+           
            
         }
+        if(Array.isArray(wordArray) && !wordArray.length ){
+            return "Aucun mot dans la phrase a une taille correspondant au nombre passé en paramètre"
+        }
         return wordArray;    
+    }else {
+        return "veuillez revoir vos parametres"
     }
    
 }
 
-console.log(countWords("je suis alle au marche", 2));
+function countWords2(phrase,word) {
+    if(typeof phrase === 'string' && typeof word === 'string'){
+        wordArray=[];
+        arr = phrase.split(' ');
+        for(let i=0; i<arr.length; i++) {
+
+            if(typeof arr[i] === 'string' && arr[i].includes(word)) {
+                wordArray.push(arr[i]);
+            }
+
+           
+           
+        }
+        if(Array.isArray(wordArray) && !wordArray.length ){
+            return "Le mot passé en paramètre n'existe pas dans la phrase"
+        }
+        return wordArray.length;    
+    }else {
+        return "veuillez revoir vos parametres"
+    }
+   
+}
+
+console.log(countWords("je suis alle au marche", 4));
+console.log(countWords2("le hosting en javascript est le fait de remonter les variables à la compilation","le"));
