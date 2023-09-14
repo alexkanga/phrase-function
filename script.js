8e0aabeba0051d7f4d15c4b4f794c602a4ef2764
alexkanga <kalexane@gmail.com>
2023-09-14 12:04:41 +0000

    Add function genSentence

--- script.js
+++ script.js
@@ -62,7 +62,63 @@ function encadrer(chaine) {
     return tableau
 }
 
+/**
+ * 
+ * Écrire une fonction qui prend en paramètre une chaîne de caractère , la fonction doit retourner la somme de tous les nombres inclus dans la chaîne.
+Ex: somme("bo2jou5) => 7
+Ex2: somme("bonjour") => 0
+ */
+function sommeNombreChaine(chaine) {
+    let numberArray = [];
+    let somme = 0;
+    if (typeof chaine === 'string') {
+        //test if parseint(chaine) return NaN
+        
 
+        chaineToArray = chaine.split('');
+        for (let i = 0; i < chaineToArray.length; i++) {
+            //parseint en base 10 , convertit le nombre chaine en nombre base decimale 10
+            if (!isNaN(parseInt(chaineToArray[i]),10)) {
+                numberArray.push(parseInt(chaineToArray[i], 10));
+            }
+           
+        }
+        //calcul de la somme a partir du tableau numberArray
+        return somme = numberArray.reduce((acc, num) => acc + num);
+        
+    }else{
+        return "Le paramètre passé n'est pas une chaine de caractère " + chaine
+    }
+   
+}
+
+//non terminé
+function genSentence(tailleChaine){
+    const voyelles = "aeiouy";
+    const consonnes = "bcdfghjklmnpqrstvwxz";
+    let chaineArray = [];
+    if(tailleChaine>1){
+        for(let i=0; i<tailleChaine; i=i+3) {
+            chaineArray.push(consonnes[Math.floor(Math.random()*20)]);
+            chaineArray.push(voyelles[Math.floor(Math.random()*6)]);
+            chaineArray.push(voyelles[Math.floor(Math.random()*6)]);
+        }
+        return  chaineArray.slice(0, tailleChaine).join("");
+        //return chaineArray.join("");
+
+    }else{
+        return "parametre taille chaine insuffisante pour génerer une chaine : " + tailleChaine
+    }
+
+    return "taille de la chaine = "+tailleChaine;
+
+}
+
+
+console.log(genSentence(8));
+
+// console.log(parseInt("5b8", 10));
+console.log(sommeNombreChaine("bonjour793"));
 console.log(encadrer("salut famille"));
 
 console.log(countWords("je suis alle au marche", 4));
